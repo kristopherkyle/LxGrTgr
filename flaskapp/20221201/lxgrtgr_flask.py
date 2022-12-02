32cfc38dcac72e69e965571797acccfba8c147a6
Kristopher Kyle <kristopherkyle1@gmail.com>
2022-12-02 14:02:55 -0800

    update flask app

--- flaskapp/20221201/lxgrtgr_flask.py
+++ flaskapp/20221201/lxgrtgr_flask.py
@@ -4,18 +4,42 @@ from flask import Flask, redirect, render_template, request, url_for
 
 import LxGrTgr_01 as lxgr
 
-version = ".01" 
+version = ".02" 
+
+class sampleTok():	
+	def __init__(self): 
+		self.idx = "" 
+		self.word = ""
+		self.lemma = ""
+		self.upos = ""
+		self.xpos = ""
+		self.deprel = ""
+		self.head = ""
+		self.headidx = ""
+		self.children = ""
+		self.lxgrtag = ""
+		self.cat1 = ""
+		self.cat2 = ""
+		self.cat3 = ""
+		self.cat4 = ""
+		self.cat5 = ""
+		self.cat6 = ""
+		self.cat7 = ""
+		self.cat8 = ""
+		self.cat9 = ""
 
 def rundata(text):
 	#print(text)
 	if len(text) == 0:
 		output = None
 	else:
-		output = [item for sublist in lxgr.tag(text) for item in sublist] #make output a flat list
-		for x in output:
-			for tag in [x.lxgrtag,x.cat1,x.cat2,x.cat3,x.cat4,x.cat5,x.cat6,x.cat7,x.cat8]:
-				if tag == None:
-					tag = ""
+		output = []
+		tagged =  lxgr.tag(text)
+		for sent in tagged:
+			for token in sent:
+				output.append(token)
+			bufferTok = sampleTok()
+			output.append(bufferTok)
 	return(output)
 
 
