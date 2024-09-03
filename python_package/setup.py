35dca552190a78f2b6f1cdea46e1050c2d5f5d39
Kristopher Kyle <kristopherkyle1@gmail.com>
2024-09-02 17:14:28 -0700

    create functional python package

---
+++ python_package/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lxgrtgr",
+    version="0.5.30",
+    author="Kristopher Kyle",
+    author_email="kristopherkyle1@gmail.com",
+    description="Lexicogrammatical tagging and tag counting tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kristopherkyle/LxGrTgr",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={"lxgrtgr": ["*.txt"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['importlib_resources'],
+)
---
+++ python_package/src/lxgrtgr/__init__.py
@@ -0,0 +1,1 @@
+from lxgrtgr.lxgrtgr import *
